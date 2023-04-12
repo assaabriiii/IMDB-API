6df59f49a2beac967e3d44c56b2ecdaa0a208cf5
B <amirsbry1942@gmail.com>
2023-04-12 14:08:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
-import sqlite3 
+from typing import Union
+from funcs import *
+from fastapi import FastAPI
+from urllib.parse import unquote
 
 
-conn = sqlite3.connect('movies/movies.db')
-cursor = conn.cursor()
+app = FastAPI()
 
 
-def close_connection():
-    conn.close()
+@app.get("/movies/name:{star}")
+def api_movies_from_star(star: str,):
+    name = unquote(star)
+    result = all_movies_from_star(name)
+    return {"name": result}
 
 
-def all_movies_from_star(name:str):
-    cursor.execute("SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE LOWER(name) = LOWER(?)))",(name,))
-    result = cursor.fetchall()
-    close_connection()
-    return result
+@app.get("/movies/random")
+def api_random_movie():
+    result = random_movie()
+    return {"movie": result}
+
+@app.get("/movies/year:{year}")
+def api_movies_from_year(year: str,):
+    year = unquote(year)
+    result = movies_from_year(year)
+    return {"movies": result}
 
 
-def random_movie(): 
-    cursor.execute('SELECT * FROM movies ORDER BY RANDOM() LIMIT 1') 
-    result = cursor.fetchone()
-    conn.close()
-    close_connection()
-    return result
+@app.get("/movies/year/before/year:{year}")
+def api_movies_before_year(year: str,):
+    year = unquote(year)
+    result = movies_from_year_before(year)
+    return {"movies": result}
 
 
-def movies_from_year(year:int):
-    cursor.execute('SELECT * FROM movies WHERE year = (?)', (year,))
-    result = cursor.fetchall()
-    return result
-
-
-def movies_from_year_before(year:int):
-    cursor.execute('SELECT * FROM movies WHERE year <= (?)', (year,))
-    result = cursor.fetchall()
-    return result
-
-
-def movies_from_year_after(year:int):
-    cursor.execute('SELECT * FROM movies WHERE year >= (?)', (year,))
-    result = cursor.fetchall()
-    return result
-
-    
+@app.get("/movies/year/after/year:{year}")
+def api_movies_before_year(year: str,):
+    year = unquote(year)
+    result = movies_from_year_after(year)
+    return {"movies": result}
